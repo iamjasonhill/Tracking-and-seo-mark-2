982abe4bb71b4d1beae31244c8dc20b98a29227e
iamjasonhill <jason@jasonhill.com.au>
2025-09-21 20:04:44 +1000

    Implement CRUD API endpoints

--- app/schemas/account.py
+++ app/schemas/account.py
@@ -1,13 +1,26 @@
+from typing import Optional
+
 from pydantic import BaseModel
+
 
 class AccountBase(BaseModel):
     provider: str
 
+
 class AccountCreate(AccountBase):
     credentials: dict
+    user_id: int
+
+
+class AccountUpdate(BaseModel):
+    provider: Optional[str] = None
+    credentials: Optional[dict] = None
+
 
 class Account(AccountBase):
     id: int
+    user_id: int
+    credentials: dict
 
     class Config:
         orm_mode = True
